239f4dbc86c6dce8e982520dff467f34dfe8a7e4
martincz <martincz@mokeedev.com>
2017-01-19 14:52:38 +0800

    klteusc: mokee-ify

--- lineage.mk
+++
@@ -1,7 +0,0 @@
-$(call inherit-product, device/samsung/klteusc/full_klteusc.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-PRODUCT_DEVICE := klteusc
-PRODUCT_NAME := lineage_klteusc
---
+++ mokee.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/samsung/klteusc/full_klteusc.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := klteusc
+PRODUCT_NAME := mk_klteusc
